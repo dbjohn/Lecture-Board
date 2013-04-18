b8f65ef260b737605c0286f38f09fa292de4a50c
c04392400 <c04392400@mydit.ie>
2013-04-18 11:49:05 +0100

    have jquery and ajax example working

--- posts.js
+++ posts.js
@@ -11,6 +11,7 @@ if (Meteor.isClient) {
       Posts.insert({
         owner: Meteor.userId(),
 	body: $body.val(),
+	votes: 0,
         created_at: Date()
       });
       $body.val('');
@@ -25,11 +26,36 @@ if (Meteor.isClient) {
   Template.list.events({
     'click .post-remove': function(event) {
       Posts.remove(this._id);
+    },
+    'click input.vote_up': function(event) {
+	    if(Posts.findOne({_id: this._id}).owner != Meteor.userId()){     //if the post is not owned by the voter, then the voter can vote for it
+ 		Posts.update({_id: this._id},{$inc: {votes:1}});
+	}
     }
   });
 
-  Template.list.posts = Posts.find({}, {sort: {created_at: -1}});
+  Template.list.posts = Posts.find({}, {sort: {votes: -1}});
+
+// alert("hello");
+Meteor.startup(function () {
+ $('#testajax').text("test 1 works");
+
+	console.log('sstuff');
+$('#test').click(function(){
+	console.log("it works");
+	$('#textajax').text("works");
+	alert("before xhr");
+	$.get('test.txt',function(data) {
+		$('#testajax').html(data);
+		alert('Load was performed.');
+	});
+ });
+});
+
+
+
 }
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
@@ -38,11 +64,17 @@ if (Meteor.isServer) {
 }
 
 Posts.allow({ 
+	//only users logged in and the owners of posts can insert or remove posts. Logged in users can update posts, for voting.
 	insert: function(userId, post){
 		return (userId && post.owner == userId);	
 	},
+	update: function(userId, post){
+                return (userId);
+        },
 	remove: function(userId, post){
-		return post.owner == userId;
+		return (userId && post.owner == userId);
 	}
 });
+
+
 
